2ee5686e8042c462e06c7f3e3c2a8919ad1c8d2a
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2024-05-28 14:18:47 +0300

    Fix nav btns

--- src/index.js
+++ src/index.js
@@ -7,8 +7,26 @@ const homeBtn = document.querySelector("#home-btn");
 const aboutBtn = document.querySelector("#about-btn");
 const menuBtn = document.querySelector("#menu-btn");
 
-homeBtn.addEventListener("click", createHomePage);
+function ActivateBtn(pressedBtn) {
+  // de-activating all btns first
+  homeBtn.classList.remove("active-btn");
+  aboutBtn.classList.remove("active-btn");
+  menuBtn.classList.remove("active-btn");
 
-menuBtn.addEventListener("click", createMenuPage);
+  pressedBtn.classList.add("active-btn");
+}
 
-aboutBtn.addEventListener("click", createAboutPage);
+homeBtn.addEventListener("click", function () {
+  ActivateBtn(homeBtn);
+  createHomePage();
+});
+
+menuBtn.addEventListener("click", function () {
+  ActivateBtn(menuBtn);
+  createMenuPage();
+});
+
+aboutBtn.addEventListener("click", function () {
+  ActivateBtn(aboutBtn);
+  createAboutPage();
+});
